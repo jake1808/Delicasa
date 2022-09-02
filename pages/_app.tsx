771aa98f86b8b1bbdcf58875b428aab34ca599f1
Jacob Mutale <mutale1808@gmail.com>
2022-09-02 22:16:37 +0200

    first commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,28 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  projectId: "delicasa",
+  storageBucket: "delicasa.appspot.com",
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId:process.env.APP_ID,
+  measurementId: process.env.MEASUREMENT_ID
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
 }
