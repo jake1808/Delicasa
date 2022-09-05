a75c84b697075a06fcdb905a8fb68fc4a9d3e3c0
Jacob Mutale <mutale1808@gmail.com>
2022-09-05 22:02:56 +0200

    nav bar responsive

--- components/nav.tsx
+++ components/nav.tsx
@@ -4,26 +4,27 @@ import Link from 'next/link'
 
 function Nav(){
     return(
-        <div className="navbar">
-            <div className="navbar-start pl-4">
-              <Link href="../pages/index.tsx">
-              <Image src="/logo.webp" width={80} height={80}  alt="logo"/>
-              </Link>
-            </div>
-            <div className='navbar-middle '>
-            <Link href="/pages/index.tsx" >
-                <a className='mr-4 hover:text-red'>Home</a>
-            </Link> 
-             <Link href="/pages/index.tsx">
-            <a className='hover:text-red'>Featured products</a>
-            </Link>
-            </div>
-            <div className="navbar-end">
-            <label tabIndex={0} className="btn btn-ghost lg:hidden">
-             <AiOutlineMenu color='black' size={30}/>
-            </label>
-            </div>
+        <div className="navbar bg-white">
+        <div className="navbar-start">
+         <Image src='/logo.webp' width={50} height={50}/>
         </div>
+        <div className="navbar-center hidden lg:flex">
+          <ul className="menu menu-horizontal ">
+            <li><a>Home</a></li>
+            <li><a>Featured products</a></li>
+          </ul>
+        </div>
+        <div className='navbar-end '>
+        <div className='dropdown'>
+        <label tabIndex={0} className="btn btn-ghost lg:hidden">
+            <AiOutlineMenu/>
+        </label>
+        <ul tabIndex={0} className="menu menu-compact">
+
+        </ul>
+        </div>
+        </div>
+      </div>
     ); 
 }
 
