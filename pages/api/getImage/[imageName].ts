ab950324bceb05473c8108f885c1dd4af65d4df6
Jacob Mutale <mutale1808@gmail.com>
2022-09-07 20:33:32 +0200

    comments

--- pages/api/getImage/[imageName].ts
+++ pages/api/getImage/[imageName].ts
@@ -1,41 +1,41 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
-import {getDownloadURL, getStorage, ref} from 'firebase/storage'
+// // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+// import type { NextApiRequest, NextApiResponse } from 'next'
+// import {getDownloadURL, getStorage, ref} from 'firebase/storage'
 
 
-type Data = {
-  url: string,
-  error:string
-}
+// type Data = {
+//   url: string,
+//   error:string
+// }
 
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<Data>
-) {
-  const {imageName} = req.query
+// export default async function handler(
+//   req: NextApiRequest,
+//   res: NextApiResponse<Data>
+// ) {
+//   const {imageName} = req.query
 
-  const storage = getStorage();
-  const pathReference = ref(storage, `images/${imageName}`);
+//   const storage = getStorage();
+//   const pathReference = ref(storage, `images/${imageName}`);
 
- await getDownloadURL(pathReference).then((url)=>{
-    res.status(200).json({ url: url, error:''})
-  })
-  .catch((error)=>{
-    switch(error.code){
-      case 'storage/object-not-found':
-        //TODO:write a response
-        return res.status(300).json({error:'Storage/object-not-found', url:''})
-        break;
-      case 'storage/unauthorized':
-        //TODO:write a response
-        return res.status(404).json({error:'storage/unauthorized', url:''})
-        break;
-        case 'storage/unknown':
-        //TODO:write a response
-        return res.status(500)
-        return res.status(404).json({error:'storage/unknown', url:''})
-        break;
-    }
-  })
+//  await getDownloadURL(pathReference).then((url)=>{
+//     res.status(200).json({ url: url, error:''})
+//   })
+//   .catch((error)=>{
+//     switch(error.code){
+//       case 'storage/object-not-found':
+//         //TODO:write a response
+//         return res.status(300).json({error:'Storage/object-not-found', url:''})
+//         break;
+//       case 'storage/unauthorized':
+//         //TODO:write a response
+//         return res.status(404).json({error:'storage/unauthorized', url:''})
+//         break;
+//         case 'storage/unknown':
+//         //TODO:write a response
+//         return res.status(500)
+//         return res.status(404).json({error:'storage/unknown', url:''})
+//         break;
+//     }
+//   })
   
-}
+// }
