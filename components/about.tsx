82a3d1581473ba103e2664f7517f094a7e97400d
Jacob Mutale <mutale1808@gmail.com>
2022-09-08 12:46:52 +0200

    about section finished

--- components/about.tsx
+++ components/about.tsx
@@ -1,6 +1,6 @@
 import { type } from "os";
 import Image from 'next/image'
-import GetImage from "../lib/data/get_image";
+import Link from "next/link";
 
 type props = {
     url:string
@@ -24,11 +24,14 @@ function About({url}:props) {
         />
             
         </div>
-        <div className="flex flex-col justify-center items-center text-center w-full">
-            <h2>About us</h2>
-            <h3>Deli-ght in every flavour</h3>
-            <p>We are a local Deli based in two locations in lusaka. We offer products from around the world, including British teas, Italian Hams, American Bakes and Coffees and the best of your local Zambian products!</p>
-            <p>We are affiliated with Majoru meats and offer a wide range of their products. We also have our own line of meats and sausage flavours from all over the globe. We offer pop up barbecues/sausage sizzles at private and public events. Additionally we are proud members of &quot;This Is Not A Game&quot;, the nationally recognized state for legal game meat distributors.</p>
+        <div className="flex flex-col md:justify-center items-center text-center w-full py-20 px-6">
+            <h2 className="font-bold text-5xl mb-4">About us</h2>
+            <h3 className="font-semibold text-3xl mb-4">Delight in every flavour</h3>
+            <p className="mb-4">We are a local Deli based in two locations in lusaka. We offer products from around the world, including British teas, Italian Hams, American Bakes and Coffees and the best of your local Zambian products!</p>
+            <p className="mb-4">We are affiliated with Majoru meats and offer a wide range of their products. We also have our own line of meats and sausage flavours from all over the globe. We offer pop up barbecues/sausage sizzles at private and public events. Additionally we are proud members of &quot;This Is Not A Game&quot;, the nationally recognized state for legal game meat distributors.</p>
+            <Link href="">
+                <a className="hover:text-primary hover:underline">Visit our stores</a>
+            </Link>
         </div>
       </div>
     );
